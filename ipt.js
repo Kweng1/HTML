be1e515dc9549f83336b0fdbd84f195e0ffebfb2
kweng1 <isidorokyle123@gmail.com>
2023-09-24 15:40:50 +0800

    nay bag-o

--- ipt.js
+++ ipt.js
@@ -1,12 +1,16 @@
+const nickname = "AE";
+const age = 20;
+const bigInt = 900719925124740999n;
+const choice = false;
+var a;
+const n = null;
+const value = Symbol('HELLO');
+const cars = ["Raptor", "Mercedes", "BMW"];
+const person = {firstName:"Aeron", lastName:"Villafuerte", age:20, eyeColor:"browns"};
+console.log(typeof bigInt);//returns bigint
+console.log(typeof age);//returns number
+console.log(typeof nickname);//returns string
+console.log(typeof choice);//returns boolean
 
-var person = {
-    FirstName : "Aicee",
-    LastName : "Villafuerte",
-    Age : 10,
-    fullName : function() {
-    return this.firstName + " " + this.lastName;
-    }
-    };
-    console.log(person.FirstName+" "+person.LastName);
-    console.log(Array.isArray(person)); //returns false
-    
+
+  
